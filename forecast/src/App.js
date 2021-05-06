2e65b504e5af1b140e14569019f2819e91f9692f
Rinju Sherpa <rinju54@gmail.com>
2021-05-06 12:05:11 -0500

    updated app.js to get weather data

--- forecast/src/App.js
+++ forecast/src/App.js
@@ -1,25 +1,56 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import moment from "moment";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      items: [],
+    };
+  }
+  async componentDidMount() {
+    await fetch(
+      "https://bg7p7w7pxd.execute-api.us-east-2.amazonaws.com/forecast/forecast"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json.data.timelines[1].intervals,
+        });
+      });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>...Loading</div>;
+    } else {
+      return (
+        <div className="main">
+          <h1>❄ 10 Day forecast 🌥</h1>
+          <div className="Weather">
+            {items.map((i) => (
+              <div key={i.startTime} className="each">
+                <h3>{moment(i.startTime).format("MMM DD, dddd")} </h3>
+                <img
+                  src={"./" + i.values.weatherCode + ".svg"}
+                  alt="weather code"
+                  className="weather-code"
+                />
+                🌡 {i.values.temperature}°F & Wind Speed: {i.values.windSpeed}
+                mph
+                <hr />
+              </div>
+            ))}
+          </div>{" "}
+          .
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
